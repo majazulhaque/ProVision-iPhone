295286c85e88f92aa63f31e46492aa33759dc851
Majaz <majaz@cyclecircle.one>
2024-08-25 15:27:10 +0530

    implement HowItWorks and Fotter section.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
-
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 import * as Sentry from "@sentry/react";
 
 const App = () => {
@@ -14,6 +15,8 @@ const App = () => {
       <Highlights />
       <Model />
       <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
   );
 };
